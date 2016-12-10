2cb74784479294e61ba2387f92181d24015c59b0
zachary <443428762@qq.com>
2016-12-10 14:07:12 +0800

    cloud functin

--- cloud.js
+++ cloud.js
@@ -1,10 +1,17 @@
 var AV = require('leanengine');
 
+var testFunc = require('./cloudFuncs/test');
+
 /**
  * 一个简单的云代码方法
  */
 AV.Cloud.define('hello', function(request, response) {
   response.success('Hello world!');
 });
+
+/**
+ * test 云函数
+ */
+AV.Cloud.define('test', testFunc.test);
 
 module.exports = AV.Cloud;
