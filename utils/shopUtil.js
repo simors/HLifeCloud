ac04dd2674f9a266dbfc09d2b7ec14910d26d672
zachary <443428762@qq.com>
2017-01-05 16:55:53 +0800

    empty judge

--- utils/shopUtil.js
+++ utils/shopUtil.js
@@ -7,84 +7,88 @@ var util = require('./util');
 
 function shopCommentFromLeancloudObject(results) {
   var shopComments = []
-  results.forEach(function(item, index) {
-    var shopComment = {}
-    shopComment.id = item.id
-    var createdAt = util.parseDate(item.createdAt)
-    shopComment.createdAt = createdAt.valueOf()
-    shopComment.createdDate = numberUtils.formatLeancloudTime(createdAt, 'YYYY-MM-DD HH:mm:SS')
-    shopComment.shopCommentTime = numberUtils.getConversationTime(createdAt.valueOf())
-    var updatedAt = util.parseDate(item.updatedAt)
-    shopComment.updatedAt = updatedAt.valueOf()
+  if(results && results.length) {
+    results.forEach(function(item, index) {
+      var shopComment = {}
+      shopComment.id = item.id
+      var createdAt = util.parseDate(item.createdAt)
+      shopComment.createdAt = createdAt.valueOf()
+      shopComment.createdDate = numberUtils.formatLeancloudTime(createdAt, 'YYYY-MM-DD HH:mm:SS')
+      shopComment.shopCommentTime = numberUtils.getConversationTime(createdAt.valueOf())
+      var updatedAt = util.parseDate(item.updatedAt)
+      shopComment.updatedAt = updatedAt.valueOf()
 
-    var attrs = item.attributes
-    shopComment.content = attrs.content
-    shopComment.blueprints = attrs.blueprints
-    shopComment.score = attrs.score
+      var attrs = item.attributes
+      shopComment.content = attrs.content
+      shopComment.blueprints = attrs.blueprints
+      shopComment.score = attrs.score
 
-    var targetShop = {}
-    var targetShopAttrs = attrs.targetShop.attributes
-    if(targetShopAttrs) {
-      targetShop.shopId = attrs.targetShop.id
-      targetShop.shopName = targetShopAttrs.shopName
-    }
-    shopComment.targetShop = targetShop
+      var targetShop = {}
+      var targetShopAttrs = attrs.targetShop.attributes
+      if(targetShopAttrs) {
+        targetShop.shopId = attrs.targetShop.id
+        targetShop.shopName = targetShopAttrs.shopName
+      }
+      shopComment.targetShop = targetShop
 
-    var user = {}
-    var userAttrs = attrs.user && attrs.user.attributes
-    if(userAttrs) {
-      user.id = attrs.user.id
-      user.nickname = userAttrs.nickname
-      user.avatar = userAttrs.avatar
-    }
-    shopComment.user = user
-    shopComments.push(shopComment)
-  })
+      var user = {}
+      var userAttrs = attrs.user && attrs.user.attributes
+      if(userAttrs) {
+        user.id = attrs.user.id
+        user.nickname = userAttrs.nickname
+        user.avatar = userAttrs.avatar
+      }
+      shopComment.user = user
+      shopComments.push(shopComment)
+    })
+  }
   return shopComments
 }
 
 function shopCommentReplyFromLeancloudObject(results) {
   var shopCommentReplys = []
-  results.forEach(function(item, index) {
-    var shopCommentReply = {}
-    shopCommentReply.id = item.id
-    var createdAt = util.parseDate(item.createdAt)
-    shopCommentReply.createdAt = createdAt.valueOf()
-    shopCommentReply.createdDate = numberUtils.formatLeancloudTime(createdAt, 'YYYY-MM-DD HH:mm:SS')
-    shopCommentReply.shopCommentReplyTime = numberUtils.getConversationTime(createdAt.valueOf())
-    var updatedAt = util.parseDate(item.updatedAt)
-    shopCommentReply.updatedAt = updatedAt.valueOf()
+  if(results && results.length) {
+    results.forEach(function(item, index) {
+      var shopCommentReply = {}
+      shopCommentReply.id = item.id
+      var createdAt = util.parseDate(item.createdAt)
+      shopCommentReply.createdAt = createdAt.valueOf()
+      shopCommentReply.createdDate = numberUtils.formatLeancloudTime(createdAt, 'YYYY-MM-DD HH:mm:SS')
+      shopCommentReply.shopCommentReplyTime = numberUtils.getConversationTime(createdAt.valueOf())
+      var updatedAt = util.parseDate(item.updatedAt)
+      shopCommentReply.updatedAt = updatedAt.valueOf()
 
-    var attrs = item.attributes
-    shopCommentReply.content = attrs.content
-    shopCommentReply.replyShopCommentId = attrs.replyShopComment.id
+      var attrs = item.attributes
+      shopCommentReply.content = attrs.content
+      shopCommentReply.replyShopCommentId = attrs.replyShopComment.id
 
-    var user = {}
-    var userAttrs = attrs.user && attrs.user.attributes
-    if(userAttrs) {
-      user.id = attrs.user.id
-      user.nickname = userAttrs.nickname
-      user.avatar = userAttrs.avatar
-    }
-    shopCommentReply.user = user
-
-    var parentReply = {}
-    var parentReplyAttrs = attrs.parentReply && attrs.parentReply.attributes
-    if(parentReplyAttrs) {
-      parentReply.id = attrs.parentReply.id
-
-      var _user = {}
-      var parentReplyAttrsUserAttrs = parentReplyAttrs.user && parentReplyAttrs.user.attributes
-      if(parentReplyAttrsUserAttrs) {
-        _user.id = attrs.user.id
-        _user.nickname = userAttrs.nickname
-        _user.avatar = userAttrs.avatar
+      var user = {}
+      var userAttrs = attrs.user && attrs.user.attributes
+      if(userAttrs) {
+        user.id = attrs.user.id
+        user.nickname = userAttrs.nickname
+        user.avatar = userAttrs.avatar
       }
-      parentReply.user = _user
-      shopCommentReply.parentReply = parentReply
-    }
-    shopCommentReplys.push(shopCommentReply)
-  })
+      shopCommentReply.user = user
+
+      var parentReply = {}
+      var parentReplyAttrs = attrs.parentReply && attrs.parentReply.attributes
+      if(parentReplyAttrs) {
+        parentReply.id = attrs.parentReply.id
+
+        var _user = {}
+        var parentReplyAttrsUserAttrs = parentReplyAttrs.user && parentReplyAttrs.user.attributes
+        if(parentReplyAttrsUserAttrs) {
+          _user.id = attrs.user.id
+          _user.nickname = userAttrs.nickname
+          _user.avatar = userAttrs.avatar
+        }
+        parentReply.user = _user
+        shopCommentReply.parentReply = parentReply
+      }
+      shopCommentReplys.push(shopCommentReply)
+    })
+  }
 
   return shopCommentReplys
 }
