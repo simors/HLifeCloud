3b463c2c7975ded2c556848b1551cb0a12433cae
reluo <21404683@qq.com>
2017-03-10 18:59:45 +0800

    shopList

--- adminCloudFuncs/shopManager/shopManager.js
+++ adminCloudFuncs/shopManager/shopManager.js
@@ -13,7 +13,7 @@ function getShopCategoryList(request, response) {
   query.find().then((results)=> {
     results.forEach((result)=> {
       var tags = []
-      console.log('containedTag', result.attributes.containedTag)
+      // console.log('containedTag', result.attributes.containedTag)
       if (result.attributes.containedTag) {
         result.attributes.containedTag.forEach((tag)=> {
           var tagInfo = {
@@ -125,130 +125,155 @@ function updateShopTag(request, response) {
   })
 }
 
-  function getShopList(request, response) {
-    var shopCategoryId = request.params.shopCategoryId
-    var sortId = request.params.sortId // 0-智能,1-按好评,2-按距离
-    var distance = request.params.distance
-    var geo = request.params.geo
-    var geoCity = request.params.geoCity
-    var isRefresh = request.params.isRefresh
+function getShopList(request, response) {
+  var shopCategoryId = request.params.shopCategoryId
+  var sortId = request.params.sortId // 0-智能,1-按好评,2-按距离
+  var geoCity = request.params.geoCity
+  var isRefresh = request.params.isRefresh
 
-    var skipNum = request.params.isRefresh ? 0 : (request.params.skipNum || 0)
-    var shopTagId = request.params.shopTagId
-    var query = new AV.Query('Shop')
+  var skipNum = request.params.isRefresh ? 0 : (request.params.skipNum || 0)
+  var shopTagId = request.params.shopTagId
+  var query = new AV.Query('Shop')
+  //用 include 告知服务端需要返回的关联属性对应的对象的详细信息，而不仅仅是 objectId
 
-    if (shopCategoryId) {
-      //构建内嵌查询
-      var innerQuery = new AV.Query('ShopCategory')
+  query.include('owner')
+  query.include('targetShopCategory')
+  query.include('containedTag')
 
-      innerQuery.equalTo('shopCategoryId', shopCategoryId)
-      //执行内嵌查询
-      query.matchesQuery('targetShopCategory', innerQuery)
+  if (shopCategoryId) {
+    //构建内嵌查询
+    var innerQuery = new AV.Query('ShopCategory')
+
+    innerQuery.equalTo('shopCategoryId', shopCategoryId)
+    //执行内嵌查询
+    query.matchesQuery('targetShopCategory', innerQuery)
+  }
+
+
+  if (sortId == 1) {
+    if (!isRefresh) { //分页查询
+      query.skip(skipNum)
+      // query.lessThanOrEqualTo('score', lastScore)
     }
+    query.addDescending('score')
+  } else if (sortId == 2) {
+    if (!isRefresh) { //分页查询
+      query.skip(skipNum)
+      // query.lessThanOrEqualTo('geo', lastGeo)
+    }
+  } else {
+    if (!isRefresh) { //分页查询
+      query.skip(skipNum)
+      // query.lessThanOrEqualTo('score', lastScore)
+    }
+    query.addDescending('score')
+    // query.addDescending('geo')
+  }
+  //query.limit(5) // 最多返回 5 条结果
 
-    //用 include 告知服务端需要返回的关联属性对应的对象的详细信息，而不仅仅是 objectId
-    query.include(['targetShopCategory', 'owner', 'containedTag'])
+  // console.log('getShopList.geoCity===', geoCity)
+  // console.log('getShopList.typeof geoCity===', typeof geoCity)
+  if (geoCity) {
+    query.contains('geoCity', geoCity)
+  }
 
-    if (sortId == 1) {
-      if (!isRefresh) { //分页查询
-        query.skip(skipNum)
-        // query.lessThanOrEqualTo('score', lastScore)
+
+  if (shopTagId) {
+    var shopTag = AV.Object.createWithoutData('ShopTag', shopTagId)
+    query.equalTo('containedTag', shopTag)
+  }
+  // console.log('getShopList.query===', query)
+  query.find().then(function (results) {
+    console.log('count', results.length)
+    // console.log('getShopList.results=', results)
+    var point = null
+    var shopList = []
+    results.forEach((result) => {
+      console.log('count', result.attributes.containedAnnouncements)
+      var tags = []
+      // console.log('containedTag', result.attributes.containedTag)
+      if (result.attributes.containedTag) {
+        result.attributes.containedTag.forEach((tag)=> {
+          var tagInfo = {
+            id: tag.id,
+            name: tag.attributes.name
+          }
+          tags.push(tagInfo)
+        })
       }
-      query.addDescending('score')
-    } else if (sortId == 2) {
-      if (!isRefresh) { //分页查询
-        query.skip(skipNum)
-        // query.lessThanOrEqualTo('geo', lastGeo)
+      var targetShopCategory={
+        text:result.attributes.targetShopCategory.attributes.text,
+        id:result.attributes.targetShopCategory.id
       }
-    } else {
-      if (!isRefresh) { //分页查询
-        query.skip(skipNum)
-        // query.lessThanOrEqualTo('score', lastScore)
+      var owner = {
+        id : result.attributes.owner.id,
+        username:result.attributes.owner.attributes.username
       }
-      query.addDescending('score')
-      // query.addDescending('geo')
-    }
-    //query.limit(5) // 最多返回 5 条结果
-    if (distance) {
-      // console.log('getShopList.geo===', geo)
-      // console.log('getShopList.Array.isArray(geo)===', Array.isArray(geo))
-      if (Array.isArray(geo)) {
-        var point = new AV.GeoPoint(geo)
-        query.withinKilometers('geo', point, distance)
+      let shop={
+        id:result.objectId,
+        shopName:result.attributes.shopName,
+        shopAddress:result.attributes.shopAddress,
+        isOpen:result.attributes.isOpen,
+        coverUrl:result.attributes.coverUrl,
+        contactNumber:result.attributes.contactNumber,
+        targetShopCategory:targetShopCategory,
+        phone:result.attributes.phone,
+        geoCity:result.attributes.geoCity,
+        name:result.attributes.name,
+        openTime:result.attributes.openTime,
+        geoDistrict:result.attributes.geoDistrict,
+        album:result.attributes.album,
+        owner:owner,
+        createdAt:result.createdAt
       }
-    } else {
-      // console.log('getShopList.geoCity===', geoCity)
-      // console.log('getShopList.typeof geoCity===', typeof geoCity)
-      if (geoCity) {
-        query.contains('geoCity', geoCity)
-      }
-      if (Array.isArray(geo)) {
-        var point = new AV.GeoPoint(geo)
-        query.withinKilometers('geo', point, 100)
-      }
-    }
-    if (shopTagId) {
-      var shopTag = AV.Object.createWithoutData('ShopTag', shopTagId)
-      query.equalTo('containedTag', shopTag)
-    }
-    // console.log('getShopList.query===', query)
-     query.find().then(function (results) {
-       console.log('count',results.length)
-      // console.log('getShopList.results=', results)
-      var point = null
-      if (Array.isArray(geo)) {
-        point = new AV.GeoPoint(geo)
-      }
-      var shopList = []
-      results.forEach((result) => {
-        result.userCurGeo = point
-        result.nextSkipNum = parseInt(skipNum) + results.length
-        shopList.push(result)
+
+      // result.nextSkipNum = parseInt(skipNum) + results.length
+      shopList.push(shop)
+    })
+    response.success(shopList)
+  }, function (err) {
+    response.error(err)
+  })
+}
+
+function updateChoosenCategory(request, response) {
+  var query = new AV.Query('ShopCategory')
+  query.notEqualTo('displaySort', null)
+  query.find().then((results)=> {
+    var categoryCancelList = []
+    results.forEach((result)=> {
+      var category = AV.Object.createWithoutData('ShopCategory', result.id)
+      category.set('displaySort', null)
+      categoryCancelList.push(category)
+    })
+    AV.Object.saveAll(categoryCancelList).then(()=> {
+      var categorys = []
+      var count = 1
+      request.params.choosenCategory.forEach((result)=> {
+        var category = AV.Object.createWithoutData('ShopCategory', result.id)
+        category.set('displaySort', count)
+        categorys.push(category)
+        count++
       })
-      response.success(shopList)
-    }, function (err) {
+      AV.Object.saveAll(categorys)
+    }).then(()=> {
+      response.success()
+    }, (err)=> {
       response.error(err)
     })
-  }
+  })
 
-  function updateChoosenCategory(request,response){
-    var query = new AV.Query('ShopCategory')
-    query.notEqualTo('displaySort',null)
-    query.find().then((results)=>{
-      var categoryCancelList=[]
-     results.forEach((result)=>{
-       var category= AV.Object.createWithoutData('ShopCategory',result.id)
-       category.set('displaySort',null)
-       categoryCancelList.push(category)
-     })
-      AV.Object.saveAll(categoryCancelList).then(()=>{
-        var categorys=[]
-        var count = 1
-        request.params.choosenCategory.forEach((result)=>{
-          var category= AV.Object.createWithoutData('ShopCategory',result.id)
-          category.set('displaySort',count)
-          categorys.push(category)
-          count++
-        })
-        AV.Object.saveAll(categorys)
-      }).then(()=>{
-        response.success()
-      },(err)=>{
-        response.error(err)
-      })
-    })
+}
 
-  }
+var ShopManagerFunc = {
+  getShopCategoryList: getShopCategoryList,
+  getShopTagList: getShopTagList,
+  createShopCategory: createShopCategory,
+  updateShopCategory: updateShopCategory,
+  createShopTag: createShopTag,
+  updateShopTag: updateShopTag,
+  getShopList: getShopList,
+  updateChoosenCategory: updateChoosenCategory
 
-  var ShopManagerFunc = {
-    getShopCategoryList: getShopCategoryList,
-    getShopTagList: getShopTagList,
-    createShopCategory: createShopCategory,
-    updateShopCategory: updateShopCategory,
-    createShopTag: createShopTag,
-    updateShopTag: updateShopTag,
-    getShopList: getShopList,
-    updateChoosenCategory:updateChoosenCategory
-
-  }
-  module.exports = ShopManagerFunc
+}
+module.exports = ShopManagerFunc
