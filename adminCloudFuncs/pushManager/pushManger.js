d5d0da6a89702cd72a251edb7b763eb850194679
zachary <443428762@qq.com>
2017-03-25 11:04:05 +0800

    messag epush

--- adminCloudFuncs/pushManager/pushManger.js
+++ adminCloudFuncs/pushManager/pushManger.js
@@ -3,17 +3,30 @@
  */
 var AV = require('leanengine');
 var Promise = require('bluebird');
+var util = require('../../utils/util');
 
 function push(request, response) {
   console.log('push------>>>>', request.params)
 
-  var query = buildPushQuery(request.params)
+  if(!request.params.pushContent) {
+    response.error({
+      code: -1,
+      message: 'pushContent is null'
+    })
+  }else {
+    var query = buildPushQuery(request.params)
+    console.log('query******<><><><>', query)
 
-  var data = {}
+    var data = buildPushData(request.params)
+    console.log('data******<><><><>', data)
 
-  _push(data, query)
+    _push(data, query)
 
-  response.success(request.params)
+    response.success({
+      code: 1,
+      message: 'push success'
+    })
+  }
 }
 
 /**
@@ -76,7 +89,7 @@ function _push(data, query) {
 }
 
 /**
- * terminalType: 1,  // 1-不限; 2-iOS; 3-Android
+ *  terminalType: 1,  // 1-不限; 2-iOS; 3-Android
  *  pushCondition: 1, //1-不限; 2-自定义
  *  inactivityDaysIsChecked: false, //是否根据未活跃天数进行推送
  *  inactivityDays: 30,  //未活跃天数
@@ -91,28 +104,135 @@ function _push(data, query) {
  *  pushContentType: 1, //1-文本; 2-JSON
  *  pushContent: '',
  *  pushFileList: [],
- * @param queryParams
+ * @param params
  * @returns {*}
  */
-function buildPushQuery(queryParams) {
-  var query = new AV.Query('_Installation')
+function buildPushData(params) {
+  var data = {}
 
-  var deviceUserInfoQuery = new AV.Query('DeviceUserInfo')
+  var pushCondition = params.pushCondition;
 
-  if(queryParams.terminalType == 2) {
-    query.equalTo('deviceType', 'ios')
-  }else if(queryParams.terminalType == 3) {
-    query.equalTo('deviceType', 'android')
-  }
+  var pushTimeType = params.pushTimeType;
+  var pushTime = params.pushTime;
 
-  if(queryParams.pushCondition == 2) {
-    if(queryParams.inactivityDaysIsChecked == 'true') {
+  var expireTimeType = params.expireTimeType;
+  var expireTime = params.expireTime;
+  var expireIntervalTime = params.expireIntervalTime;
+  var expireIntervalTimeUnit = params.expireIntervalTimeUnit;
 
+  var pushContentType = params.pushContentType;
+  var pushContent = params.pushContent;
+  var message_cover_url = params.message_cover_url;
+
+  if(pushCondition == 2) {
+    if(pushTimeType == 2) {
+      data.push_time = new Date(pushTime)
+    }
+
+    if(expireTimeType == 2) {
+      data.expiration_time = new Date(expireTime)
+    }else if (expireTimeType == 3) {
+      var expiration_interval = 1;
+      if(expireIntervalTimeUnit == 2) {
+        expiration_interval = parseInt(expireIntervalTime) * 24 * 60 * 60;
+      }else {
+        expiration_interval = parseInt(expireIntervalTime) * 60 * 60;
+      }
+      data.expiration_interval = expiration_interval;
     }
   }
 
+  if(pushContentType == 2) {
+    // console.log('pushContent....---->>>', pushContent)
+    // console.log('pushContent....---->>>', typeof pushContent)
+    Object.assign(data, pushContent);
+    // console.log('data....--****-->>>', data)
+  }else {
+    data.alert = pushContent;
+  }
 
+  data.message_cover_url = message_cover_url;
 
+  return data;
+}
+
+/**
+ *  terminalType: 1,  // 1-不限; 2-iOS; 3-Android
+ *  pushCondition: 1, //1-不限; 2-自定义
+ *  inactivityDaysIsChecked: false, //是否根据未活跃天数进行推送
+ *  inactivityDays: 30,  //未活跃天数
+ *  pushTimeType: 1,   //1-现在; 2-指定时间
+ *  pushTime: '',      //推送时间
+ *  expireTimeType: 1, //1-从不; 2-指定时间; 3-指定间隔时间
+ *  expireTime: '',    //过期时间
+ *  expireIntervalTime: '1', //过期间隔时间
+ *  expireIntervalTimeUnit: '1', //过期间隔时间单位; 1-小时; 2-天
+ *  pushTargetUserType: '1', //1-不限; 2-店主; 3-推广员
+ *  pushTargetDistrict: [],
+ *  pushContentType: 1, //1-文本; 2-JSON
+ *  pushContent: '',
+ *  pushFileList: [],
+ * @param params
+ * @returns {*}
+ */
+function buildPushQuery(params) {
+  var query = new AV.Query('_Installation')
+
+  var terminalType = params.terminalType;
+  var pushCondition = params.pushCondition;
+  var inactivityDaysIsChecked = params.inactivityDaysIsChecked;
+  var inactivityDays = params.inactivityDays;
+  var pushTargetUserType = params.pushTargetUserType;
+  var pushTargetDistrict = params.pushTargetDistrict;
+
+  if(terminalType == 2) {
+    query.equalTo('deviceType', 'ios')
+  }else if(terminalType == 3) {
+    query.equalTo('deviceType', 'android')
+  }
+
+  if(pushCondition == 2) {
+    if(inactivityDaysIsChecked) {
+      if(inactivityDays) {
+        var now = new Date();
+        var newDate = util.DateAdd('day', -parseInt(inactivityDays), now);
+        // console.log('newDate====<><><>', newDate)
+        query.lessThanOrEqualTo('updatedAt', newDate);
+      }
+    }
+  }
+
+  if(pushTargetUserType == 2 || pushTargetUserType == 3
+    || (pushTargetDistrict && pushTargetDistrict.length)) {
+
+    var userTypeQuery = new AV.Query('_User');
+    if(pushTargetUserType == 2) {
+      userTypeQuery.equalTo('identity', 'shopkeeper');
+    }else if(pushTargetUserType == 3) {
+      userTypeQuery.equalTo('identity', 'promoter');
+    }
+
+    var districtOrQuery = new AV.Query('_User');
+    if(pushTargetDistrict && pushTargetDistrict.length) {
+      var districtQueryArr = []
+      pushTargetDistrict.forEach(function(item){
+        var areaInfoArr = item.split('-');
+        var districtQuery = new AV.Query('_User');
+        if('1' == areaInfoArr[0]) {
+          districtQuery.equalTo('geoProvinceCode', areaInfoArr[1])
+        }else if('2' == areaInfoArr[0]) {
+          districtQuery.equalTo('geoCityCode', areaInfoArr[1])
+        }
+        districtQueryArr.push(districtQuery);
+      })
+      districtOrQuery = AV.Query.or.apply(null, districtQueryArr)
+    }
+
+    var andQuery = AV.Query.and(userTypeQuery, districtOrQuery);
+    query.matchesQuery('owner', andQuery);
+  }
+
+  return query;
 }
 
 var pushManageFunc = {
