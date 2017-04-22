19bd46b954d0e0e5984be380e992ea74a0e7cc60
yang24201 <yang24201@126.com>
2017-04-22 16:55:45 +0800

    add city and province promoter performance statistics

--- cloudFuncs/Statistics/PerformanceStat.js
+++ cloudFuncs/Statistics/PerformanceStat.js
@@ -12,12 +12,61 @@ var promoterFuncs = require('../Promoter')
 const INVITE_PROMOTER = 1       // 邀请推广员获得的收益
 const INVITE_SHOP = 2           // 邀请店铺获得的收益
 
+// 统计级别
+const LEVEL_DISTRICT = 1
+const LEVEL_CITY = 2
+const LEVEL_PROVINCE = 3
+
 var job = schedule.scheduleJob('0 0 12 * * *', function() {
   var date = (new Date()).toLocaleDateString()
-  runPromoterStat(date)
+  runDistrictPromoterStat(date).then((districtStat) => {
+    return runCityPromoterStat(districtStat)
+  }).then((cityStat) => {
+    return runProvincePromoterStat(cityStat)
+  }).catch((err) => {
+    console.log(err)
+  })
 })
 
-function runPromoterStat(date) {
+/**
+ * 执行统计数据保存，如果已经有相关统计数据则更新
+ * @param data
+ */
+function execStatSave(data) {
+  var query = new AV.Query('PromoterPerformanceStat')
+  query.equalTo('province', data.province)
+  query.equalTo('city', data.city)
+  query.equalTo('district', data.district)
+  query.equalTo('level', data.level)
+
+  return query.first().then((stat) => {
+    // 已经存在相关统计数据
+    if (stat) {
+      var newStat = AV.Object.createWithoutData('PromoterPerformanceStat', stat.id)
+      newStat.set('shopNum', stat.shopNum)
+      newStat.set('promoterNum', stat.promoterNum)
+      newStat.set('earning', stat.earn)
+      return newStat.save()
+    } else {
+      var PerformanceStat = AV.Object.extend('PromoterPerformanceStat')
+      var performanceStat = new PerformanceStat()
+      performanceStat.set('province', data.province)
+      performanceStat.set('city', data.city)
+      performanceStat.set('district', data.district)
+      performanceStat.set('shopNum', data.shopNum)
+      performanceStat.set('promoterNum', data.promoterNum)
+      performanceStat.set('earning', data.earn)
+      performanceStat.set('level', data.level)
+      return performanceStat.save()
+    }
+  })
+}
+
+/**
+ * 执行区县级推广统计
+ * @param date
+ */
+function runDistrictPromoterStat(date) {
   var mysqlConn = undefined
   var sql = ''
   var originalRecords = []
@@ -98,15 +147,16 @@ function runPromoterStat(date) {
     })
     var saveOps = []
     statMap.forEach((stat, key) => {
-      var PerformanceStat = AV.Object.extend('PromoterPerformanceStat')
-      var performanceStat = new PerformanceStat()
-      performanceStat.set('province', key.province)
-      performanceStat.set('city', key.city)
-      performanceStat.set('district', key.district)
-      performanceStat.set('shopNum', stat.shopNum)
-      performanceStat.set('promoterNum', stat.promoterNum)
-      performanceStat.set('earning', stat.earn)
-      saveOps.push(performanceStat.save())
+      var statData = {
+        province: key.province,
+        city: key.city,
+        district: key.district,
+        shopNum: stat.shopNum,
+        promoterNum: stat.promoterNum,
+        earn: stat.earn,
+        level: LEVEL_DISTRICT,
+      }
+      saveOps.push(execStatSave(statData))
     })
     return Promise.all(saveOps)
   }).catch((err) => {
@@ -118,12 +168,130 @@ function runPromoterStat(date) {
   })
 }
 
+/**
+ * 执行城市推广员业绩统计
+ * @param districtStat
+ */
+function runCityPromoterStat(districtStat) {
+  var statMap = new Map()
+  var keyArray = []
+  districtStat.forEach((stat) => {
+    var areaKey = {
+      province: stat.attributes.province,
+      city: stat.attributes.city,
+    }
+    var key = keyArray.find((value) => {
+      return (value.province == areaKey.province && value.city == areaKey.city) ? true : false
+    })
+    var areaStat = undefined
+    if (key) {
+      areaStat = statMap.get(key)
+    } else {
+      areaStat = undefined
+      key = areaKey
+      keyArray.push(key)
+    }
+    if (!areaStat) {
+      areaStat = {
+        shopNum: 0,
+        promoterNum: 0,
+        earn: 0,
+      }
+      areaStat.shopNum = stat.attributes.shopNum
+      areaStat.promoterNum = stat.attributes.promoterNum
+      areaStat.earn = stat.attributes.earning
+    } else {
+      areaStat.shopNum += stat.attributes.shopNum
+      areaStat.promoterNum += stat.attributes.promoterNum
+      areaStat.earn += stat.attributes.earning
+    }
+    statMap.set(key, areaStat)
+  })
+
+  var saveOps = []
+  statMap.forEach((stat, key) => {
+    var statData = {
+      province: key.province,
+      city: key.city,
+      shopNum: stat.shopNum,
+      promoterNum: stat.promoterNum,
+      earn: stat.earn,
+      level: LEVEL_CITY,
+    }
+    saveOps.push(execStatSave(statData))
+  })
+  return Promise.all(saveOps)
+}
+
+/**
+ * 执行省份推广业绩统计
+ * @param cityStat
+ * @returns {Promise.<*>}
+ */
+function runProvincePromoterStat(cityStat) {
+  var statMap = new Map()
+  var keyArray = []
+  cityStat.forEach((stat) => {
+    var areaKey = {
+      province: stat.attributes.province,
+    }
+    var key = keyArray.find((value) => {
+      return (value.province == areaKey.province) ? true : false
+    })
+    var areaStat = undefined
+    if (key) {
+      areaStat = statMap.get(key)
+    } else {
+      areaStat = undefined
+      key = areaKey
+      keyArray.push(key)
+    }
+    if (!areaStat) {
+      areaStat = {
+        shopNum: 0,
+        promoterNum: 0,
+        earn: 0,
+      }
+      areaStat.shopNum = stat.attributes.shopNum
+      areaStat.promoterNum = stat.attributes.promoterNum
+      areaStat.earn = stat.attributes.earning
+    } else {
+      areaStat.shopNum += stat.attributes.shopNum
+      areaStat.promoterNum += stat.attributes.promoterNum
+      areaStat.earn += stat.attributes.earning
+    }
+    statMap.set(key, areaStat)
+  })
+
+  var saveOps = []
+  statMap.forEach((stat, key) => {
+    var statData = {
+      province: key.province,
+      shopNum: stat.shopNum,
+      promoterNum: stat.promoterNum,
+      earn: stat.earn,
+      level: LEVEL_PROVINCE,
+    }
+    saveOps.push(execStatSave(statData))
+  })
+  return Promise.all(saveOps)
+}
+
+/**
+ * 执行推广统计
+ * @param request
+ * @param response
+ */
 function statPromoterPerformance(request, response) {
   var date = request.params.date
   if (!date) {
     date = new Date().toLocaleDateString()
   }
-  runPromoterStat(date).then(() => {
+  runDistrictPromoterStat(date).then((districtStat) => {
+    return runCityPromoterStat(districtStat)
+  }).then((cityStat) => {
+    return runProvincePromoterStat(cityStat)
+  }).then(() => {
     response.success({errcode: 0, message: '执行推广员日统计功能成功'})
   }).catch((err) => {
     console.log(err)
