0854c720ccbb739b456b90d68f4daf3ae6561adc
wanpeng <wanpeng19860919@163.com>
2017-05-06 21:20:51 +0800

    <feat> 修改download下载页面

--- routes/download.js
+++ routes/download.js
@@ -5,11 +5,24 @@
 var router = require('express').Router();
 var AV = require('leanengine');
 
+var AppVersion = AV.Object.extend('AppVersion');
+
 
 // 查询 Todo 列表
 router.get('/', function(req, res, next) {
-  res.render('download', {
 
+  var query = new AV.Query(AppVersion)
+  query.limit(1)
+  query.descending('createdAt')
+  query.first().then((result) => {
+    var fileUrl = result.attributes.fileUrl
+    res.render('download', {
+      androidAppDownloadUrl: fileUrl || 'https://www.pgyer.com/pn66'
+    })
+  }).catch(() => {
+    res.render('download', {
+      androidAppDownloadUrl: 'https://www.pgyer.com/pn66'
+    })
   })
 })
 
