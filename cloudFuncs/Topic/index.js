97ed8909afb21b6f6851357882dc5beaa4d896cb
reluo <21404683@qq.com>
2017-07-12 22:46:54 +0800

    topic tiaozheng

--- cloudFuncs/Topic/index.js
+++ cloudFuncs/Topic/index.js
@@ -5,7 +5,7 @@ var AV = require('leanengine');
 var Promise = require('bluebird')
 var topicUtil = require('../../utils/topicUtil');
 var util = require('../../utils/util');
-
+var numberUtils = require('../../utils/numberUtils')
 function disableTopicByUser(request,response){
   var id = request.params.id
   var topic = AV.Object.createWithoutData('Topics',id)
@@ -178,32 +178,8 @@ function fetchTopicList(request,response){
 		results.forEach((result)=>{
 			var position = result.attributes.position
 			var user = result.attributes.user
-			topicList.push({
-				content: result.attributes.content, //话题内容
-				title: result.attributes.title,
-				abstract:result.attributes.abstract,
-				imgGroup: result.attributes.imgGroup, //图片
-				objectId: result.id,  //话题id
-				categoryId: result.attributes.category.id,  //属于的分类
-				categoryName: result.attributes.category.attributes.title, // 话题分类名
-				nickname: user?result.attributes.user.attributes.nickname:undefined, //所属用户昵称
-				username: user?result.attributes.user.attributes.username:undefined, //所属用户昵称
-				userId:user?result.attributes.user.id:undefined,     // 所属用户的id
-				createdAt: result.createdAt,  //创建时间
-				avatar: user?result.attributes.user.attributes.avatar:undefined,  //所属用户头像
-				commentNum: result.attributes.commentNum, //评论数
-				likeCount: result.attributes.likeCount, //点赞数
-				address : position?position.address:undefined,
-				city : position?position.city:undefined,
-				longitude : position?position.longitude:undefined,
-				latitude : position?position.latitude:undefined,
-				streetNumber : position?position.streetNumber:undefined,
-				street : position?position.street:undefined,
-				province : position?position.province:undefined,
-				country : position?position.country:undefined,
-				district : position?position.district:undefined,
-				userUpdatedAt : user?user.updatedAt:undefined,
-			})
+			var topic = topicUtil.newTopicFromLeanCloudObject(result)
+			topicList.push(topic)
 		})
 		response.success(topicList)
 	},  (err)=> {
@@ -384,35 +360,7 @@ function fetchTopicCommentsV2(request,response){
 			var parentComment = result.attributes.parentComment
 			var replyComment = result.attributes.replyComment
       var user = result.attributes.user
-			var topicComment = {
-				content: result.attributes.content,
-				commentId : result.id,
-				topicId : result.attributes.topic.id,
-				parentCommentContent : parentComment?result.attributes.parentComment.attributes.content:undefined,
-				parentCommentUserName : parentComment?result.attributes.parentComment.attributes.user.attributes.username:undefined,
-				parentCommentNickname : parentComment?result.attributes.parentComment.attributes.user.attributes.nickname:undefined,
-				parentCommentId : parentComment?result.attributes.parentComment.id:undefined,
-				replyCommentContent : replyComment?result.attributes.replyComment.attributes.content:undefined,
-				replyCommentUserName : replyComment?result.attributes.replyComment.attributes.user.attributes.username:undefined,
-				replyCommentNickname : replyComment?result.attributes.replyComment.attributes.user.attributes.nickname:undefined,
-				replyCommentId : replyComment?result.attributes.replyComment.id:undefined,
-				upCount : result.attributes.likeCount,
-				authorUsername : user?result.attributes.user.attributes.username:undefined,
-				authorNickname : user?result.attributes.user.attributes.nickname:undefined,
-				commentCount : result.attributes.commentCount,
-				authorId : user?result.attributes.user.id:undefined,
-				authorAvatar : user?user.attributes.avatar:undefined,
-				createdAt : result.createdAt,
-				address : position?position.address:undefined,
-				city : position?position.city:undefined,
-				longitude : position?position.longitude:undefined,
-				latitude : position?position.latitude:undefined,
-				streetNumber : position?position.streetNumber:undefined,
-				street : position?position.street:undefined,
-				province : position?position.province:undefined,
-				country : position?position.country:undefined,
-				district : position?position.district:undefined
-			}
+			var topicComment = topicUtil.newTopicCommentFromLeanCloudObject(result)
 			// console.log('result===<',result.id)
 			allComments.push(topicComment)
 			commentList.push(topicComment.commentId)
@@ -536,35 +484,7 @@ function pubulishTopicComment(request,response){
 						var position = result.attributes.position
 						var parentComment = result.attributes.parentComment
             var user = result.attributes.user
-						var commentInfo = {
-              content: result.attributes.content,
-              commentId : result.id,
-              topicId : result.attributes.topic.id,
-              parentCommentContent : parentComment?result.attributes.parentComment.attributes.content:undefined,
-              parentCommentUserName : parentComment?result.attributes.parentComment.attributes.user.attributes.username:undefined,
-              parentCommentNickname : parentComment?result.attributes.parentComment.attributes.user.attributes.nickname:undefined,
-              parentCommentId : parentComment?result.attributes.parentComment.id:undefined,
-              replyCommentContent : replyComment?result.attributes.replyComment.attributes.content:undefined,
-              replyCommentUserName : replyComment?result.attributes.replyComment.attributes.user.attributes.username:undefined,
-              replyCommentNickname : replyComment?result.attributes.replyComment.attributes.user.attributes.nickname:undefined,
-              replyCommentId : replyComment?result.attributes.replyComment.id:undefined,
-              upCount : result.attributes.likeCount,
-              authorUsername : user?result.attributes.user.attributes.username:undefined,
-              authorNickname : user?result.attributes.user.attributes.nickname:undefined,
-              commentCount : result.attributes.commentCount,
-              authorId : user?result.attributes.user.id:undefined,
-              authorAvatar : user?user.attributes.avatar:undefined,
-              createdAt : result.createdAt,
-              address : position?position.address:undefined,
-              city : position?position.city:undefined,
-              longitude : position?position.longitude:undefined,
-              latitude : position?position.latitude:undefined,
-              streetNumber : position?position.streetNumber:undefined,
-              street : position?position.street:undefined,
-              province : position?position.province:undefined,
-              country : position?position.country:undefined,
-              district : position?position.district:undefined
-						}
+						var commentInfo = topicUtil.newTopicCommentFromLeanCloudObject(result)
 						response.success(commentInfo)
 					},(err)=>{
 						response.error(err)
@@ -582,35 +502,7 @@ function pubulishTopicComment(request,response){
 					var parentComment = result.attributes.parentComment
 					var replyComment = result.attributes.replyComment
           var user = result.attributes.user
-					var commentInfo = {
-            content: result.attributes.content,
-            commentId : result.id,
-            topicId : result.attributes.topic.id,
-            parentCommentContent : parentComment?result.attributes.parentComment.attributes.content:undefined,
-            parentCommentUserName : parentComment?result.attributes.parentComment.attributes.user.attributes.username:undefined,
-            parentCommentNickname : parentComment?result.attributes.parentComment.attributes.user.attributes.nickname:undefined,
-            parentCommentId : parentComment?result.attributes.parentComment.id:undefined,
-            replyCommentContent : replyComment?result.attributes.replyComment.attributes.content:undefined,
-            replyCommentUserName : replyComment?result.attributes.replyComment.attributes.user.attributes.username:undefined,
-            replyCommentNickname : replyComment?result.attributes.replyComment.attributes.user.attributes.nickname:undefined,
-            replyCommentId : replyComment?result.attributes.replyComment.id:undefined,
-            upCount : result.attributes.likeCount,
-            authorUsername : user?result.attributes.user.attributes.username:undefined,
-            authorNickname : user?result.attributes.user.attributes.nickname:undefined,
-            commentCount : result.attributes.commentCount,
-            authorId : user?result.attributes.user.id:undefined,
-            authorAvatar : user?user.attributes.avatar:undefined,
-            createdAt : result.createdAt,
-            address : position?position.address:undefined,
-            city : position?position.city:undefined,
-            longitude : position?position.longitude:undefined,
-            latitude : position?position.latitude:undefined,
-            streetNumber : position?position.streetNumber:undefined,
-            street : position?position.street:undefined,
-            province : position?position.province:undefined,
-            country : position?position.country:undefined,
-            district : position?position.district:undefined
-					}
+					var commentInfo = topicUtil.newTopicCommentFromLeanCloudObject(result)
 					response.success(commentInfo)
 				},(err)=>{
 					response.error(err)
--- utils/topicUtil.js
+++ utils/topicUtil.js
@@ -94,9 +94,81 @@ function topicCommentFromLeancloudObject(lcObj) {
   return topicComment
 }
 
+function newTopicCommentFromLeanCloudObject(result){
+
+	var position = result.attributes.position
+	var parentComment = result.attributes.parentComment
+	var replyComment = result.attributes.replyComment
+	var user = result.attributes.user
+	var comment = {
+		content: result.attributes.content,
+		commentId : result.id,
+		topicId : result.attributes.topic.id,
+		parentCommentContent : parentComment?result.attributes.parentComment.attributes.content:undefined,
+		parentCommentUserName : parentComment?result.attributes.parentComment.attributes.user.attributes.username:undefined,
+		parentCommentNickname : parentComment?result.attributes.parentComment.attributes.user.attributes.nickname:undefined,
+		parentCommentId : parentComment?result.attributes.parentComment.id:undefined,
+		replyCommentContent : replyComment?result.attributes.replyComment.attributes.content:undefined,
+		replyCommentUserName : replyComment?result.attributes.replyComment.attributes.user.attributes.username:undefined,
+		replyCommentNickname : replyComment?result.attributes.replyComment.attributes.user.attributes.nickname:undefined,
+		replyCommentId : replyComment?result.attributes.replyComment.id:undefined,
+		upCount : result.attributes.likeCount,
+		authorUsername : user?result.attributes.user.attributes.username:undefined,
+		authorNickname : user?result.attributes.user.attributes.nickname:undefined,
+		commentCount : result.attributes.commentCount,
+		authorId : user?result.attributes.user.id:undefined,
+		authorAvatar : user?user.attributes.avatar:undefined,
+		createdAt : result.createdAt,
+		address : position?position.address:undefined,
+		city : position?position.city:undefined,
+		longitude : position?position.longitude:undefined,
+		latitude : position?position.latitude:undefined,
+		streetNumber : position?position.streetNumber:undefined,
+		street : position?position.street:undefined,
+		province : position?position.province:undefined,
+		country : position?position.country:undefined,
+		district : position?position.district:undefined,
+		createdDate : numberUtils.formatLeancloudTime(createdAt, 'YYYY-MM-DD')
+	}
+	return comment
+}
+
+function newTopicFromLeanCloudObject(result) {
+	var position = result.attributes.position
+	var user = result.attributes.user
+	var topic = {
+		content: result.attributes.content, //话题内容
+		title: result.attributes.title,
+		abstract:result.attributes.abstract,
+		imgGroup: result.attributes.imgGroup, //图片
+		objectId: result.id,  //话题id
+		categoryId: result.attributes.category.id,  //属于的分类
+		categoryName: result.attributes.category.attributes.title, // 话题分类名
+		nickname: user?result.attributes.user.attributes.nickname:undefined, //所属用户昵称
+		username: user?result.attributes.user.attributes.username:undefined, //所属用户昵称
+		userId:user?result.attributes.user.id:undefined,     // 所属用户的id
+		createdAt: result.createdAt,  //创建时间
+		avatar: user?result.attributes.user.attributes.avatar:undefined,  //所属用户头像
+		commentNum: result.attributes.commentNum, //评论数
+		likeCount: result.attributes.likeCount, //点赞数
+		address : position?position.address:undefined,
+		city : position?position.city:undefined,
+		longitude : position?position.longitude:undefined,
+		latitude : position?position.latitude:undefined,
+		streetNumber : position?position.streetNumber:undefined,
+		street : position?position.street:undefined,
+		province : position?position.province:undefined,
+		country : position?position.country:undefined,
+		district : position?position.district:undefined,
+		userUpdatedAt : user?user.updatedAt:undefined,
+	}
+	return topic
+}
 var topicUtil = {
   topicCommentFromLeancloudObject: topicCommentFromLeancloudObject,
   upFromLeancloudObject: upFromLeancloudObject,
+	newTopicCommentFromLeanCloudObject: newTopicCommentFromLeanCloudObject,
+	newTopicFromLeanCloudObject: newTopicFromLeanCloudObject,
 }
 
 module.exports = topicUtil
