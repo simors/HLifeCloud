1e3ace327beed979aad0e9c3336b2c6cffa5dc0c
yang24201 <yang24201@126.com>
2017-07-17 17:12:12 +0800

    modify new user guide reply

--- mpFuncs/Server/index.js
+++ mpFuncs/Server/index.js
@@ -53,10 +53,26 @@ var generateQrcode = function (req, res, next) {
 }
 
 var newUserGuide = function (req, res, next) {
-  res.reply({
-    type: 'text',
-    content: '请查看新手指引'
-  })
+  res.reply([
+    {
+      title: '汇邻优店推广规则介绍',
+      description: '如何用人脉成为高富帅、白富美',
+      picurl: 'http://www.07073.com/robotuploads/20170715/s1twjtse4cl.jpg',
+      url: 'http://chanye.07073.com/guonei/1648610.html'
+    },
+    {
+      title: '汇邻优店店铺入驻说明',
+      description: '如何用人脉成为高富帅、白富美',
+      picurl: 'http://inews.gtimg.com/newsapp_bt/0/1781612513/641',
+      url: 'http://news.qq.com/a/20170717/010820.htm'
+    },
+    {
+      title: '手机app下载',
+      description: '如何用人脉成为高富帅、白富美',
+      picurl: 'http://img2.utuku.china.com/640x0/news/20170715/b3cf1b7c-0ac7-4b9a-abf8-74d964ff207b.jpg',
+      url: 'http://news.china.com/news100/11038989/20170716/30974415.html'
+    },
+  ])
 }
 
 var exeClickEvent = {
