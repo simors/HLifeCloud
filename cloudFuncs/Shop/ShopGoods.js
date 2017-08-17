51e839f525573a6e69f435b4b5ea03c68dfab78a
yang24201 <yang24201@126.com>
2017-08-17 14:41:41 +0800

    modify construct shop goods method

--- cloudFuncs/Shop/ShopGoods.js
+++ cloudFuncs/Shop/ShopGoods.js
@@ -16,12 +16,14 @@ function constructShopGoods(goods) {
   }
   shopGoods.id = goods.id
   shopGoods.goodsName = shopGoodsAttr.goodsName
+  shopGoods.targetShop = shopGoodsAttr.targetShop.id
   shopGoods.price = shopGoodsAttr.price
   shopGoods.originalPrice = shopGoodsAttr.originalPrice
   shopGoods.coverPhoto = shopGoodsAttr.coverPhoto
   shopGoods.album = shopGoodsAttr.album
   shopGoods.detail = shopGoodsAttr.detail
   shopGoods.status = shopGoodsAttr.status
+  shopGoods.updatedAt = goods.updatedAt
   return shopGoods
 }
 
