a83a09629234930db10adec6b6329dd19a3fb86a
reluo <21404683@qq.com>
2017-11-17 21:01:16 +0800

    add count

--- cloudFuncs/ShopV2/UserAddress.js
+++ cloudFuncs/ShopV2/UserAddress.js
@@ -11,40 +11,50 @@ const addrStatus = {
   DISABLE_ADDR: 0,    // 被删除地址
 }
 
-async function createAddr(req,res) {
-  let {params,currentUser} = req
-  if(!currentUser){
+const addrMaxNum = 10
+
+async function createAddr(req, res) {
+  let {params, currentUser} = req
+  if (!currentUser) {
     res.error('don t login')
     return
-  }else{
-    let {username,  mobilePhoneNumber, province, city, district, addr, tag } = params
-    let Address = AV.Object.extend('Address')
-    let address = new Address()
-    address.set('admin',currentUser)
-    address.set('username', username)
-    address.set('mobilePhoneNumber', mobilePhoneNumber)
-    address.set('province', province)
-    address.set('city', city)
-    address.set('district', district)
-    address.set('addr', addr)
-    address.set('tag', tag)
-    address.set('status',addrStatus.ENABLE_ADDR)
-    try{
-      let addressInfo = await address.save()
-      res.success(constructAddress(addressInfo))
-    }catch(err){
+  } else {
+    let {username, mobilePhoneNumber, province, city, district, addr, tag} = params
+    let queryMaxNum = new AV.Query('_User')
+    try {
+      let user = await queryMaxNum.get(currentUser.id)
+      if (user.attributes.addressCount >= addrMaxNum) {
+        res.error('超过地址最大数量，请删除多余地址！')
+        return
+      } else {
+        let Address = AV.Object.extend('Address')
+        let address = new Address()
+        address.set('admin', currentUser)
+        address.set('username', username)
+        address.set('mobilePhoneNumber', mobilePhoneNumber)
+        address.set('province', province)
+        address.set('city', city)
+        address.set('district', district)
+        address.set('addr', addr)
+        address.set('tag', tag)
+        address.set('status', addrStatus.ENABLE_ADDR)
+        currentUser.increment('addressCount', 1)
+        await currentUser.save()
+        let addressInfo = await address.save()
+        res.success(constructAddress(addressInfo))
+      }
+    } catch (err) {
       res.error(err)
     }
   }
-
 }
 
-async function updateAddr(req,res) {
-  let {params,currentUser} = req
-  if(!currentUser){
+async function updateAddr(req, res) {
+  let {params, currentUser} = req
+  if (!currentUser) {
     res.error('don t login')
   }
-  let {username, mobilePhoneNumber, province, city, district, addr, tag, addrId, status } = params
+  let {username, mobilePhoneNumber, province, city, district, addr, tag, addrId, status} = params
   let address = AV.Object.createWithoutData('Address', addrId)
   address.set('username', username)
   address.set('mobilePhoneNumber', mobilePhoneNumber)
@@ -54,13 +64,13 @@ async function updateAddr(req,res) {
   address.set('addr', addr)
   address.set('tag', tag)
   address.set('status', status)
-  try{
+  try {
     await address.save()
     let query = new AV.Query('Address')
     query.include('admin')
     let addressInfo = await query.get(addrId)
     res.success(constructAddress(addressInfo))
-  }catch(err){
+  } catch (err) {
     res.error(err)
   }
 }
@@ -72,96 +82,97 @@ async function updateAddr(req,res) {
  * lastCreatedAt: str
  * @returns {Array}
  */
-async function getAddrsFunc(params){
+async function getAddrsFunc(params) {
   let {user, lastCreatedAt} = params
   let query = new AV.Query('Address')
-  query.equalTo('admin',user)
-  if(lastCreatedAt){
+  query.equalTo('admin', user)
+  if (lastCreatedAt) {
     query.lessThan('createdAt', new Date(lastCreatedAt))
   }
   query.notEqualTo('status', addrStatus.DISABLE_ADDR)
   query.include(['admin'])
   query.descending('createdAt')
-  try{
+  try {
     let addrs = await query.find()
     let addrList = []
-    if(addrs && addrs.length>0){
-      addrs.forEach((item)=>{
-        addrList.push(constructAddress(item,true))
+    if (addrs && addrs.length > 0) {
+      addrs.forEach((item)=> {
+        addrList.push(constructAddress(item, true))
       })
     }
     return addrList
-  }catch(err){
+  } catch (err) {
     return err
   }
 }
 
-async function getAddrs(req,res) {
-  let {currentUser,params} = req
-  if(!currentUser){
+async function getAddrs(req, res) {
+  let {currentUser, params} = req
+  if (!currentUser) {
     res.error('don t login')
   }
   let payload = {
     user: currentUser,
     ...params
   }
-  try{
+  try {
     let addrList = await getAddrsFunc(payload)
     res.success(addrList)
-  }catch (err){
+  } catch (err) {
     res.error(err)
   }
 }
 
-async function disableAddr(req,res){
-  let {currentUser,params} = req
-  if(!currentUser){
+async function disableAddr(req, res) {
+  let {currentUser, params} = req
+  if (!currentUser) {
     res.error('don t login')
   }
   let {addrId} = params
-  let addr = AV.Object.createWithoutData('Address',addrId)
-  addr.set('status',addrStatus.DISABLE_ADDR)
-  try{
+  let addr = AV.Object.createWithoutData('Address', addrId)
+  addr.set('status', addrStatus.DISABLE_ADDR)
+  try {
+    currentUser.increment('addressCount', 1)
+    await currentUser.save()
     await addr.save()
     res.success()
-  }catch(err){
+  } catch (err) {
     res.error(err)
   }
 }
 
 
-async function setDefaultAddr(req,res){
-  let {currentUser,params} = req
-  if(!currentUser){
+async function setDefaultAddr(req, res) {
+  let {currentUser, params} = req
+  if (!currentUser) {
     res.error('don t login')
   }
 
   let {addrId} = params
   let query = new AV.Query('Address')
-  query.equalTo('admin',currentUser)
+  query.equalTo('admin', currentUser)
   query.equalTo('status', addrStatus.DEFAUT_ADDR)
-  try{
+  try {
     let oldDefault = await query.find()
-    if(oldDefault&&oldDefault.length>0){
+    if (oldDefault && oldDefault.length > 0) {
       let oldAddr = AV.Object.createWithoutData('Address', oldDefault[0].id)
       oldAddr.set('status', addrStatus.ENABLE_ADDR)
       await oldAddr.save()
     }
     let addr = AV.Object.createWithoutData('Address', addrId)
     addr.set('status', addrStatus.DEFAUT_ADDR)
-    let addrInfo = await addr.save()
-    res.success(constructAddress(addrInfo))
-  }catch(err){
+    await addr.save()
+    res.success()
+  } catch (err) {
     res.error(err)
   }
 
 }
 
 
-async function testAddFunc(req,res){
-  return await getAddrs(req,res)
+async function testAddFunc(req, res) {
+  return await getAddrs(req, res)
 }
-
 
 
 const addrFunc = {
